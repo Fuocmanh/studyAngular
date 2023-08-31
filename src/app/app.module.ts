b46aeab1ebd971fc256c9e8b178079576ab5fe89
FuMeng <nguyenphuocmanh2003@gmail.com>
2023-08-31 12:32:24 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './client/pages/login/login.component';
+import { HomeComponent } from './client/pages/home/home.component';
+import { InputComponent } from './client/component/input/input.component';
+import { ButtonComponent } from './client/component/button/button.component';
+import { HeaderComponent } from './client/shared/header/header.component';
+import { RegisterComponent } from './client/pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    InputComponent,
+    ButtonComponent,
+    HeaderComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
